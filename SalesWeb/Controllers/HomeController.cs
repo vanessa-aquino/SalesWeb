31c6a6d06ce51ddb7d74b152e822322bdfbe2794
Vanessa <vanessaquinoo@hotmail.com>
2025-04-15 11:52:13 -0300

    style: finished the navbar and started styling the home page

--- SalesWeb/Controllers/HomeController.cs
+++ SalesWeb/Controllers/HomeController.cs
@@ -22,11 +22,6 @@ namespace SalesWeb.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
